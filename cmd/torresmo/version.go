cdad84696dbcda056f1103e2f4d99c9e04c120ed
Murilo Santana <mvrilo@gmail.com>
2021-06-03 15:17:59 -0300

    cmd: add subcommand version

--- cmd/torresmo/version.go
+++ cmd/torresmo/version.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Torresmo version",
 	Run: func(cmd *cobra.Command, args []string) {
-		println(Version)
+		fmt.Printf("v%s (%s)", Version, Commit)
 	},
 }
